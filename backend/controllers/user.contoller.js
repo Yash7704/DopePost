526e498fc5ef442a7801743e42dabb9e2f10aeb2
Yash Pandey <yashpandey30kp@gmail.com>
2025-05-18 00:04:28 +0530

    Populated the posts

--- backend/controllers/user.contoller.js
+++ backend/controllers/user.contoller.js
@@ -3,6 +3,7 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import getDataUri from "../utils/datauri.js";
 import cloudinary from "../utils/cloudinary.js";
+import { Post } from "../models/post.model.js";
 
 export const register = async (req, res) => {
   try {
@@ -64,6 +65,15 @@ export const login = async (req, res) => {
       expiresIn: "1d",
     });
     //Populate each post id in the post array
+
+    const populatedPosts = await Promise.all(
+      user.posts.map(async (postId) => {
+        const post = await Post.findById(postId);
+        if (post.author.equals(user._id)) return post;
+        return null;
+      })
+    );
+
     user = {
       _id: user._id,
       username: user.username,
@@ -72,7 +82,7 @@ export const login = async (req, res) => {
       bio: user.bio,
       followers: user.followers,
       following: user.following,
-      posts: user.posts,
+      posts: populatedPosts,
     };
 
     return res
